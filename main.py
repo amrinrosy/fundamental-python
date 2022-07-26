e2c9c2b8e83c2084d18e35011cd9d5d71e430757
amrinrosy <amrinrosy@gmail.com>
2022-07-26 23:56:16 +0700

    sampe sekuensial

--- main.py
+++ main.py
@@ -12,12 +12,26 @@ print('Ibu menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
 print("maka Budi berangkat ke toko")
 
 #Percabangan
-jumlah_botol_susu = 173;
-
+jumlah_botol_susu = 173
+jumlah_telur = 1587
+print(f"jumlah botol susu {jumlah_botol_susu} botol")
+print(f"jumlah telur {jumlah_telur} butir")
 if jumlah_botol_susu > 0:
     print("Budi mengecek harganya, dan ternyata uangnya cukup")
-    print("Budi membeli 1 botol susu")
+    if jumlah_telur == 0:
+        print("Budi membeli 1 botol susu")
+    else:
+        print("Budi membeli 1 botol susu & 6 butir telur")
 else:
     print("Budi tidak jadi membeli 1 botol susu")
 print("Budi pulang ke rumah")
 print("Menyampaikan hasilnya kepada Ibu")
+
+
+#Hitung Luas Segitiga
+print("MENGHITUNG LUAS SEGITIGA")
+print("=================================")
+alas = input("masukkan alas :")
+tinggi = input("masukkan tinggi :")
+L = 0.5 * int(alas) * int(tinggi)
+print(f"Luas segitiga dengan alas : {alas} tinggi : {tinggi} adalah {L}")
