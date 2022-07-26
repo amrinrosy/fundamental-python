f62e7f45d63571421326bd9a149f6112d1a86335
amrinrosy <amrinrosy@gmail.com>
2022-07-26 23:03:08 +0700

    sampe sekuensial

--- main.py
+++ main.py
@@ -5,7 +5,19 @@ Semua sintaksis dasar bahasa pemrograman terdiri dari :
 - Perulangan  : mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
 '''
 
+#Sekuensial
 print('Ibu berkata, "Pergi ke toko"')
 print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
 print('Ibu menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
 print("maka Budi berangkat ke toko")
+
+#Percabangan
+jumlah_botol_susu = 173;
+
+if jumlah_botol_susu > 0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+print("Budi pulang ke rumah")
+print("Menyampaikan hasilnya kepada Ibu")
