120a7291ce6ac5932e82920633c4e51e19ebbdcf
amrinrosy <amrinrosy@gmail.com>
2022-07-30 14:23:05 +0700

    sampe sekuensial daftar belanja

---
+++ For_bacabuku.py
@@ -0,0 +1,12 @@
+#Perulangan For - Baca Buku
+
+jumlah_buku = 10
+print('Ibu berkata "Baca semua bukumu"')
+jumlah_buku_bacaan = 0
+print(f'jumlah buku yang sudah dibaca {jumlah_buku_bacaan}')
+for jumlah_buku_bacaan in range (1, jumlah_buku+1):
+    print(f"Buku ke {jumlah_buku_bacaan} sudah dibaca")
+
+
+
+
---
+++ While_bacabuku.py
@@ -0,0 +1,12 @@
+#Perulangan While Baca BUku
+jumlah_buku = 10
+print('Ibu berkata "Baca semua bukumu"')
+
+jumlah_buku_bacaan = 0
+print(f'Jumlah buku yang sudah dibaca {jumlah_buku_bacaan}')
+
+while jumlah_buku_bacaan < jumlah_buku :
+    jumlah_buku_bacaan = jumlah_buku_bacaan + 1
+    print(f"Buku ke {jumlah_buku_bacaan} sudah dibaca")
+
+print(f'Jumlah buku yang sudah dibaca {jumlah_buku_bacaan}')
